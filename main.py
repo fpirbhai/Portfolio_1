a736d4e6513d335f04f7f632aa9e51b828840dd1
pirbhai14 <pirbhai14@hotmail.com>
2022-11-30 01:11:10 +0400

    Add image and links

--- main.py
+++ main.py
@@ -18,14 +18,20 @@ content2 ="""Below you can find some of my apps I have built in python. Please f
 """
 st.text(content2)
 
-col3, col4 = st.columns(2)
+col3,empty_col ,col4 = st.columns([1.5,0.5,1.5])
 
 df = pd.read_csv('data.csv', sep=';')
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/'+ row['image'])
+        st.text(f"[Source Code](row['url'])")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/'+ row['image'])
+        st.write(f"[Source Code]({row['url']})")
